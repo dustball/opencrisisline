aadaf8d753ca27dafa61f77d04a8919172bff38a
Tom Moore <tmoore@betazi.com>
2020-06-14 21:49:41 -0700

    replaced mysql with PDO corrected logAndDie error messages

--- verify.php
+++ verify.php
@@ -16,10 +16,16 @@
        
     # Uncomment this line for testing purposes (only message the admin)
     #$sql = "select * from $table_name where handle='$admin_handle'";
-    
-    $result = mysql_query($sql) or logAndDie("Failed Query #V103: ".mysql_error());
-    
-    while ($row = mysql_fetch_assoc($result)) {
+
+    try {
+        $result = $db->query($sql);
+    }
+    catch (PDOException $e) {
+        logAndDie("Failed Query #V103: " . $e->getMessage() .'->'.
+            (int)$e->getCode() . array('exception' => $e));
+    }
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         $name = trim(ucfirst($row['handle']));
         $to = '+1' . $row['phone'];
         $m1 = "$name, thank you for volunteering with the $system_name system!  This is a yearly check to see if we still have (1/2)";
@@ -29,8 +35,14 @@
         sms0($from,$to,$m1);
         sms0($from,$to,$m2);
         $sql2 = "update $table_name set verified='?' where phone='".$row['phone']."'";
-        $result2 = mysql_query($sql2) or logAndDie("Failed Query #V104: ".mysql_error());
-        sleep(1);        
+        try {
+            $result2 = $db->query($sql2);
+        }
+        catch (PDOException $e) {
+            logAndDie("Failed Query #V104: " . $e->getMessage() .'->'.
+                (int)$e->getCode() . array('exception' => $e));
+        }
+        sleep(1);
     }
     
     
